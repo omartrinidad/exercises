ad8a92cd371c9e601c174f39abdd7f116c072d07
Omar Trinidad Gutiérrez Méndez <omar.vpa@gmail.com>
2015-11-08 20:05:46 +0100

    Finished task 3

--- Sheet02/src/sheet02.cpp
+++ Sheet02/src/sheet02.cpp
@@ -13,7 +13,7 @@ void part3();
 void part4();
 void part5();
 void drawArrow(cv::Mat &image, cv::Point p, cv::Scalar color, double scaleMagnitude, double scaleArrowHead, double magnitube, double orientationDegrees);
-
+void matchingImage(Mat& img, Mat& matching, int y, int x, int rows, int cols);
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -170,7 +170,6 @@ void part2()
     cv::destroyAllWindows();
 }
 
-
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -191,7 +190,7 @@ void part3()
     cv::Mat im_Traffic_BGR = cv::imread("./images/traffic.jpg");
 
     // Blur to denoise
-    cv::GaussianBlur(im_Traffic_BGR, im_Traffic_BGR, cv::Size(3,3), 0, 0, cv::BORDER_DEFAULT);
+    cv::GaussianBlur(im_Traffic_BGR, im_Traffic_BGR, cv::Size(7,7), 0, 0, cv::BORDER_DEFAULT);
 
     // BGR to Gray
     cv::Mat im_Traffic_Gray;
@@ -199,29 +198,27 @@ void part3()
 
     // Perform the computations asked in the exercise sheet
     cv::Mat horizontal, vertical, sobel;
-    cv::Mat magnitude, angle; 
     cv::Mat magnitude_, angle_; 
 
     Sobel(im_Traffic_Gray, horizontal, CV_32F, 1, 0);
     Sobel(im_Traffic_Gray, vertical, CV_32F, 0, 1);
+    //sobel = abs(vertical) + abs(horizontal);
 
-    sobel = abs(vertical) + abs(horizontal);
-
-
-    /* cartToPolar1 */
+    /* cartToPolar OpenCV
+    cv::Mat magnitude, angle; 
     cartToPolar(horizontal, vertical, magnitude, angle);
+    */
 
-    /* cartToPolar2 */
-
+    /* cartToPolar2 own implementation */
     int rows = im_Traffic_Gray.rows;
     int cols = im_Traffic_Gray.cols;
     magnitude_ = Mat::zeros(rows, cols, CV_32F);
     angle_ = Mat::zeros(rows, cols, CV_32F);
     
-    /* improve this section with pointers */
+    /* it is possible to improve this section with pointers */
     for (int y = 0; y < rows; ++y) {
         for (int x = 0; x < cols; ++x) {
-            float mag = sqrt(pow(horizontal.at<float>(y, x), 2) + pow(horizontal.at<float>(y, x), 2));
+            float mag = sqrt(pow(horizontal.at<float>(y, x), 2) + pow(vertical.at<float>(y, x), 2));
             magnitude_.at<float>(y, x) = mag;
             float ang = atan2(horizontal.at<float>(y, x), vertical.at<float>(y, x)) * (180/M_PI);
             angle_.at<float>(y, x) = ang;
@@ -231,23 +228,35 @@ void part3()
     // Show results
     // using **cv::imshow and cv::waitKey()** and when necessary **std::cout**
     // In the end, after the last cv::waitKey(), use **cv::destroyAllWindows()**
-    /*
-    imshow("Gray image", im_Traffic_Gray);
-    imshow("Vertical", vertical);
-    imshow("Horizontal", horizontal);
-    imshow("Sobel", sobel);
-    */
-
-    imshow("Magnitud", magnitude);
-    imshow("Angle", angle);
-    imshow("Magnitud_", magnitude_);
-    imshow("Angle_", angle_);
+    
+    imshow("Magnitude", magnitude_);
+    waitKey();
+    cv::destroyAllWindows();
+    imshow("Angle", angle_);
+    waitKey();
+    cv::destroyAllWindows();
 
     // Use the function **drawArrow** provided at the end of this file in order to
     // draw Vectors showing the Gradient Magnitude and Orientation
     // (to avoid clutter, draw every 10nth gradient,
     // only if the magnitude is above a threshold)
+    
+    /* Draw the gradients in the original image */
+    Point point;
+    for (int y = 0; y < rows; y+=10) {
+        for (int x = 0; x < cols; x+=10) {
 
+            point.x = x;
+            point.y = y;
+
+            if (magnitude_.at<float>(y, x) > 32) {
+                double an = angle_.at<float>(y, x);
+                drawArrow(im_Traffic_BGR, point, Scalar(0, 130, 255), 3.555, 1.20, 1.25, an);
+            }
+        }
+    }
+
+    imshow("Gradients", im_Traffic_BGR);
     waitKey(0);
     cv::destroyAllWindows();
 }
@@ -270,15 +279,20 @@ void part4()
 
     cv::Mat im_Traffic_BGR = cv::imread("./images/traffic.jpg");
     // BGR to Gray
-    cv::Mat                       im_Traffic_Gray;
+    cv::Mat im_Traffic_Gray, im_Canny;
     cv::cvtColor( im_Traffic_BGR, im_Traffic_Gray, cv::COLOR_BGR2GRAY );
 
     // Perform edge detection as described in the exercise sheet
+    Canny(im_Traffic_BGR, im_Canny, 10, 100, 3);
 
     // Show results
     // using **cv::imshow and cv::waitKey()** and when necessary **std::cout**
-    // In the end, after the last cv::waitKey(), use **cv::destroyAllWindows()**
+    // In the end, after the last cv::waitkey(), use **cv::destroyAllWindows()**
+    
+    imshow("gray", im_Traffic_Gray);
+    imshow("canny", im_Canny);
 
+    waitKey(0);
     cv::destroyAllWindows();
 }
 
@@ -288,7 +302,6 @@ void part4()
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
 
 void part5()
 {
@@ -300,25 +313,60 @@ void part5()
     std::cout <<                                                            std::endl;
 
     // Read image, Blur to denoise
-    cv::Mat                           im_Traffic_BGR = cv::imread("./images/traffic.jpg");
-    cv::GaussianBlur( im_Traffic_BGR, im_Traffic_BGR,  cv::Size(3,3), 0, 0, cv::BORDER_DEFAULT );
+    cv::Mat im_Traffic_BGR = cv::imread("./images/traffic.jpg");
+    cv::GaussianBlur(im_Traffic_BGR, im_Traffic_BGR,  cv::Size(3,3), 0, 0, cv::BORDER_DEFAULT );
+
     // BGR to Gray
-    cv::Mat                           im_Traffic_Gray;
-    cv::cvtColor(     im_Traffic_BGR, im_Traffic_Gray, cv::COLOR_BGR2GRAY );
+    cv::Mat im_Traffic_Gray, im_Edges;
+    cv::cvtColor(im_Traffic_BGR, im_Traffic_Gray, cv::COLOR_BGR2GRAY);
 
     // Read Template
     cv::Mat im_Sign_BGR = cv::imread("./images/sign.png");
-    cv::Mat im_Sign_Gray;
+    cv::Mat im_Sign_Gray, im_Sign;
     // BGR to Gray
     cv::cvtColor( im_Sign_BGR, im_Sign_Gray, cv::COLOR_BGR2GRAY ); // cv::COLOR_BGR2GRAY // CV_BGR2GRAY
 
     // Perform the steps described in the exercise sheet
+    Mat dist;
+
+    int distType = DIST_L2;
+    int maskSize = DIST_MASK_5;
+
+    Canny(im_Traffic_Gray, im_Edges, 10, 600, 3);
+    Canny(im_Sign_Gray, im_Sign, 10, 200, 3);
+
+    //imshow("Gray", im_Traffic_Gray);
+    //imshow("Canny", im_Edges);
+
+    distanceTransform(im_Edges, dist, distType, maskSize);
+
+    int rows = dist.rows;
+    int cols = dist.cols;
+    int r = im_Sign.rows;
+    int c = im_Sign.cols;
+
+    //imshow("Dist", dist);
+    //imshow("Canny sign", im_Sign);
+    Mat result;
+
+    cout << dist.type() << endl;
+    cout << im_Sign_Gray.type() << endl;
+
+    Mat newSign;
+    im_Sign_Gray.convertTo(newSign, 5);
+
+    cout << dist.type() << endl;
+    cout << newSign.type() << endl;
+
+    matchTemplate(dist, newSign, result, CV_TM_SQDIFF );
+    imshow("Result", result);
 
     // Show results
     // using **cv::imshow and cv::waitKey()** and when necessary **std::cout**
     // In the end, after the last cv::waitKey(), use **cv::destroyAllWindows()**
     // If needed perform normalization of the image to be displayed
 
+    waitKey(0);
     cv::destroyAllWindows();
 }
 
@@ -331,11 +379,9 @@ void part5()
 
 
 // Use this function for visualizations in part3
-
 void drawArrow(cv::Mat &image, cv::Point p, cv::Scalar color, double scaleMagnitude, double scaleArrowHead, double magnitube, double orientationDegrees)
 {
     int arrowHeadAngleCoeff = 10;
-
     magnitube *= scaleMagnitude;
 
     double theta = orientationDegrees * M_PI / 180; // rad
@@ -345,6 +391,7 @@ void drawArrow(cv::Mat &image, cv::Point p, cv::Scalar color, double scaleMagnit
 
     //Draw the principle line
     cv::line(image, p, q, color);
+
     //compute the angle alpha
     double angle = atan2((double)p.y-q.y, (double)p.x-q.x);
     //compute the coordinates of the first segment
@@ -352,13 +399,14 @@ void drawArrow(cv::Mat &image, cv::Point p, cv::Scalar color, double scaleMagnit
     p.y = (int) ( q.y + static_cast<int>(  round( scaleArrowHead * sin(angle + M_PI/arrowHeadAngleCoeff)) )  );
     //Draw the first segment
     cv::line(image, p, q, color);
+
     //compute the coordinates of the second segment
     p.x = (int) ( q.x + static_cast<int>(  round( scaleArrowHead * cos(angle - M_PI/arrowHeadAngleCoeff)) )  );
     p.y = (int) ( q.y + static_cast<int>(  round( scaleArrowHead * sin(angle - M_PI/arrowHeadAngleCoeff)) )  );
     //Draw the second segment
     cv::line(image, p, q, color);
-}
 
+}
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
