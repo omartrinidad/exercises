9b3b51ca55341f312f43b6d717aedecc43087ff6
shinhokang <wis.shinho.kang@gmail.com>
2016-05-15 14:47:47 +0200

    PAC

--- artificial_life/PAC/PAC.py
+++ artificial_life/PAC/PAC.py
@@ -1,33 +1,42 @@
-
-
 class F:
     def __init__(self, params, x0, y0, steps):
         self.params = params
-        self.x[0] = x0
-        self.y[0] = y0
+        self.x = [x0]
+        self.y = [y0]
         self.steps = steps
 
     def do_function(self, step=0):
 
         if step < self.steps:
-            self.x[step+1] = self.x[step] + self.params['a'] * self.x[step] + self.params['b'] * self.y[step] + \
+            x = self.x[step] + self.params['a'] * self.x[step] + self.params['b'] * self.y[step] + \
                              self.params['e'] * self.x[step] * self.x[step] + self.params['g'] * self.x[step] * self.y[step]
-            self.y[step+1] = self.y[step] + self.params['c'] * self.x[step] + self.params['d'] * self.y[step] + \
+            self.x.append(x)
+            y = self.y[step] + self.params['c'] * self.x[step] + self.params['d'] * self.y[step] + \
                              self.params['f'] * self.y[step] * self.y[step] + self.params['h'] * self.x[step] * self.y[step]
+            self.y.append(y)
             self.do_function(step + 1)
 
     def __repr__(self):
-        s = "<parameters a:%s b:%s c:%s d:%s e:%s f:%s g:%>" % (self.a, self.b)
-        for p in self.params:
-            s += p.name
-        return text
+        # s = "a:%s b:%s" % (self.params['a'], self.params['b'])
+        s = "<parameters a:%s b:%s c:%s d:%s e:%s f:%s g:%s h:%s>" % (self.params['a'], self.params['b'], self.params['c'], \
+            self.params['d'], self.params['e'], self.params['f'], \
+            self.params['g'], self.params['h'])
+        return s
 
+    def printResult(self):
+        print "X"
+        for i in self.x:
+            print "%s " % i
+        print "Y"
+        for i in self.y:
+            print "%s " % i
 
-parameters = {'a': 0.1, 'b':0.1, 'c':0.1, 'd':0.1, 'e':0.1, 'f':0.1, 'g':0, 'h':0}
-x0 = 1
-y0 = 1
+parameters = {'a': 0.1, 'b': -0.1, 'c': 0.1, 'd': -0.1, 'e': 0.1, 'f': 0.1, 'g': 0.0, 'h': 0.0}
+x0 = 1.0
+y0 = 1.0
 steps = 10
 X = F(params=parameters, x0=x0, y0=y0, steps=steps)
-X.do_function(0)
 
 print (X)
+X.do_function(0)
+X.printResult()
