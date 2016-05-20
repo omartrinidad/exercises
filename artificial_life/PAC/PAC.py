d42f19e7b345a345f2cec8a4a38aba932867865d
Omar Gutiérrez <omar.vpa@gmail.com>
2016-05-20 14:34:41 +0200

    GNUPlot script to plot the values

--- artificial_life/PAC/PAC.py
+++ artificial_life/PAC/PAC.py
@@ -8,19 +8,25 @@ class F:
     def do_function(self, step=0):
 
         if step < self.steps:
-            x = self.x[step] + self.params['a'] * self.x[step] + self.params['b'] * self.y[step] + \
-                             self.params['e'] * self.x[step] * self.x[step] + self.params['g'] * self.x[step] * self.y[step]
+            x = self.x[step] + self.params['a'] * self.x[step] +\
+                self.params['b'] * self.y[step] +\
+                self.params['e'] * self.x[step] * self.x[step]
+                # + self.params['g'] * self.x[step] * self.y[step]
             self.x.append(x)
-            y = self.y[step] + self.params['c'] * self.x[step] + self.params['d'] * self.y[step] + \
-                             self.params['f'] * self.y[step] * self.y[step] + self.params['h'] * self.x[step] * self.y[step]
+
+            y = self.y[step] + self.params['c'] * self.x[step] +\
+                self.params['d'] * self.y[step] +\
+                self.params['f'] * self.y[step] * self.y[step]
+                # + self.params['h'] * self.x[step] * self.y[step]
             self.y.append(y)
+
             self.do_function(step + 1)
 
     def __repr__(self):
-        # s = "a:%s b:%s" % (self.params['a'], self.params['b'])
-        s = "<parameters a:%s b:%s c:%s d:%s e:%s f:%s g:%s h:%s>" % (self.params['a'], self.params['b'], self.params['c'], \
-            self.params['d'], self.params['e'], self.params['f'], \
-            self.params['g'], self.params['h'])
+        s = "<parameters a:%s b:%s c:%s d:%s e:%s f:%s g:%s h:%s>" % \
+                (self.params['a'], self.params['b'], self.params['c'], \
+                self.params['d'], self.params['e'], self.params['f'], \
+                self.params['g'], self.params['h'])
         return s
 
     def printResult(self):
@@ -29,13 +35,22 @@ class F:
         print "Y:"
         print [i for i in self.y]
 
+    def save_on_file(self):
+        results = open("results.csv", "w+")
+        for i, (x, y) in enumerate(zip(self.x, self.y)):
+            results.write("{}, {}, {}\n".format(i, x, y))
+        results.close()
 
-parameters = {'a': 0.1, 'b': -0.5, 'c': 0.1, 'd': -0.2, 'e': 0.1, 'f': 0.1, 'g': 0.0, 'h': 0.0}
-x0 = 100.0
-y0 = 30.0
+
+parameters = {
+              'a': 1.6, 'b': -1, 'c': -1, 'd': 1.6, 'e': 0.01, 'f': 0.01,
+              #'g': 0.0, 'h': 0.0
+             }
+x0 = 100
+y0 = 50
 steps = 10
-X = F(params=parameters, x0=x0, y0=y0, steps=steps)
 
-print (X)
+X = F(params=parameters, x0=x0, y0=y0, steps=steps)
 X.do_function(0)
 X.printResult()
+X.save_on_file()
---
+++ artificial_life/PAC/plotter.gp
@@ -0,0 +1,8 @@
+set grid
+set title "Predator-Prey Model"
+set xrange [0:200]
+set xlabel "Steps"
+set ylabel "Population"
+plot 'results.csv' u 1:2 w lp t 'prey',\
+     'results.csv' u 1:3 w lp t 'predator'
+pause -1
