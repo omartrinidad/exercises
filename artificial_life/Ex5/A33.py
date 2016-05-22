b3484b149d777c046fe04b0cf15392f0930d3163
shinhokang <wis.shinho.kang@gmail.com>
2016-05-22 16:09:03 +0200

    A33 test code

---
+++ artificial_life/Ex5/A33.py
@@ -0,0 +1,28 @@
+import numpy as np
+import math
+import csv
+
+cells = list(np.zeros(101))
+
+cells[0] = 42000
+cells[100] = 420
+
+gens = [list(cells)]
+alpha = 0.33
+
+for i in range(0, 100):
+    cells2 = list(cells)
+    for j in range(1,100):
+        cells[j] = min(math.floor((cells2[j-1] + cells2[j] + cells2[j+1]) * alpha), 65535)
+
+    gens.append(list(cells))
+
+# output = open("A33.csv", 'w')
+
+with open('A33.csv', 'w') as output:
+    for gen in gens:
+        for item in gen:
+            output.write("%s " % item)
+        output.write("\n")
+
+# output.close()
